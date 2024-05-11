0b610a92c95a36366677823610990fde08e4a451
alenfador2 <129333585+alenfador2@users.noreply.github.com>
2024-05-11 12:04:40 +0200

    update 0.3 added postsSchema

---
+++ models/posts.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+
+const postSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: [true, "Title is required"],
+      minLength: 2,
+      maxLength: 50,
+    },
+
+    content: {
+      type: String,
+      required: [true, "Content is required"],
+      minLength: 5,
+      maxLength: 2000,
+    },
+    imageUrl: {
+      type: String,
+    },
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: "users",
+    },
+  },
+  {
+    versionKey: false,
+    timestamps: true,
+  }
+);
+
+postsSchema.static.getAll = () => {
+  Posts.find().lean();
+};
+
+const Posts = mongoose.model("posts", postSchema);
