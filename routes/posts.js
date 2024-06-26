ab869062c1cd24fc4e50451d694abaaf2067f057
alenfador2 <129333585+alenfador2@users.noreply.github.com>
2024-06-26 17:40:22 +0200

    added route

--- routes/posts.js
+++ routes/posts.js
@@ -2,6 +2,6 @@ const express = require("express");
 const router = express.Router();
 const postsController = require("../controllers/postsController");
 
-router.get("/", postsController.get);
+router.get("/posts", postsController.get);
 
 module.exports = router;
