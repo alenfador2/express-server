982cf68515336a282ebf9a304b9da337671c6a98
alenfador2 <129333585+alenfador2@users.noreply.github.com>
2024-07-23 22:05:31 +0200

    changed app.js file

--- app.js
+++ app.js
@@ -1,19 +1,20 @@
-const express = require("express");
-const cors = require("cors");
-const logger = require("morgan");
-const postsRouter = require("./routes/posts");
+const express = require('express');
+const cors = require('cors');
+const logger = require('morgan');
+const postsRouter = require('./routes/posts');
 
 const app = express();
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
-app.use("/", postsRouter);
+app.use('/', postsRouter);
 
 app.use((err, req, res, next) => {
   console.error(err.stack);
   res.json({
-    status: "failed",
+    status: 'failed',
     code: 500,
     message: `Internal server error ${err.message}, error: ${err.stack}`,
   });
