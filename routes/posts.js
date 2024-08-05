59f5ce7f0a71489022c96c849ae6927ce4ef5c42
alenfador2 <129333585+alenfador2@users.noreply.github.com>
2024-08-05 22:42:37 +0200

    changed routes

--- routes/posts.js
+++ routes/posts.js
@@ -4,6 +4,6 @@ const postsController = require('../controllers/postsController');
 const upload = require('../middlewares/upload');
 
 router.get('/posts', postsController.get);
-router.post('/posts', upload.single('image'), postsController.post);
+router.post('/posts', upload.single('file'), postsController.post);
 
 module.exports = router;
