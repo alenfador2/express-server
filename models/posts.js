07ef23f69c95e73a1139e36b778ccdbf7409da41
alenfador2 <129333585+alenfador2@users.noreply.github.com>
2024-09-21 19:44:11 +0200

    changed model

--- models/posts.js
+++ models/posts.js
@@ -1,22 +1,22 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const postsSchema = new Schema(
   {
     title: {
       type: String,
-      required: [true, "Title is required"],
+      required: [true, 'Title is required'],
       minLength: 2,
       maxLength: 50,
     },
 
     content: {
       type: String,
-      required: [true, "Content is required"],
+      required: [true, 'Content is required'],
       minLength: 5,
       maxLength: 2000,
     },
-    imageUrl: {
+    file: {
       type: String,
       required: false,
     },
@@ -31,6 +31,6 @@ const postsSchema = new Schema(
 //   return this.find().lean();
 // };
 
-const Posts = mongoose.model("Posts", postsSchema, "posts");
+const Posts = mongoose.model('Posts', postsSchema, 'posts');
 
 module.exports = Posts;
