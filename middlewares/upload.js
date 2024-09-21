d6adfe5a3c62ad868b05ed81ee31ac7781aa5e72
alenfador2 <129333585+alenfador2@users.noreply.github.com>
2024-09-21 19:22:54 +0200

    fixed upload.js

--- middlewares/upload.js
+++ middlewares/upload.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 const store = multer.diskStorage({
   destination(req, file, cb) {
-    cb(null, path.join(__dirname, '../public/images'));
+    cb(null, path.join(__dirname, '../public/images/'));
   },
   filename(req, file, cb) {
     cb(null, Date.now() + '_' + file.originalname);
