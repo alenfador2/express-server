6f27690e407e9fd2705244d931259441dfc9f0c3
alenfador2 <129333585+alenfador2@users.noreply.github.com>
2024-10-02 21:47:30 +0200

    revert changes

--- middlewares/upload.js
+++ middlewares/upload.js
@@ -2,30 +2,30 @@ const fs = require('fs');
 const multer = require('multer');
 const path = require('path');
 
-// const imageDir = path.join(__dirname, 'public/images');
+const imageDir = path.join(__dirname, 'public/images');
 
-// if (!fs.existsSync(imageDir)) {
-//   fs.mkdirSync(imageDir, { recursive: true });
-// }
-
-// const storage = multer.diskStorage({
-//   destination(req, file, cb) {
-//     cb(null, imageDir);
-//   },
-//   filename(req, file, cb) {
-//     cb(null, Date.now() + '_' + path.extname(file.originalname));
-//   },
-// });
+if (!fs.existsSync(imageDir)) {
+  fs.mkdirSync(imageDir, { recursive: true });
+}
 
 const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, '/public/images');
+  destination(req, file, cb) {
+    cb(null, imageDir);
   },
-  filename: function (req, file, cb) {
-    const uniqueFileName = Date.now() + '-' + Math.floor(Math.random() * 1e9);
-    cb(null, file.fieldname + '-' + uniqueFileName);
+  filename(req, file, cb) {
+    cb(null, Date.now() + '_' + path.extname(file.originalname));
   },
 });
+
+// const storage = multer.diskStorage({
+//   destination: function (req, file, cb) {
+//     cb(null, '/public/images');
+//   },
+//   filename: function (req, file, cb) {
+//     const uniqueFileName = Date.now() + '-' + Math.floor(Math.random() * 1e9);
+//     cb(null, file.fieldname + '-' + uniqueFileName);
+//   },
+// });
 const fileTypes = /jpeg|jpg|png|gif/;
 const typeFilter = (req, file, cb) => {
   const mimetype = fileTypes.test(file.mimetype);
