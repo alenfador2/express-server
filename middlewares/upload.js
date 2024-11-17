1576089ff0e23c393d55b7e86857c06452b04119
alenfador2 <129333585+alenfador2@users.noreply.github.com>
2024-11-17 18:18:05 +0100

    small fixes

--- middlewares/upload.js
+++ middlewares/upload.js
@@ -2,7 +2,7 @@ const { S3Client } = require('@aws-sdk/client-s3');
 const { Upload } = require('@aws-sdk/lib-storage');
 
 const s3 = new S3Client({
-  region: process.env.AWS_REGION,
+  region: process.env.AWS_REGION || 'eu-north-1',
   credentials: {
     accessKeyId: process.env.AWS_ACCESS_KEY_ID,
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
