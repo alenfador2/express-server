88582848ffdc2370f244d19729b1af26567857a9
alenfador2 <129333585+alenfador2@users.noreply.github.com>
2025-05-12 23:19:22 +0200

    small fixes v2

--- validation/userSchema.js
+++ validation/userSchema.js
@@ -1,14 +1,14 @@
 const Joi = require('joi');
 
 const userSchema = Joi.object({
-  first_name: Joi.string().alphanum().min(3).max(30),
-  last_name: Joi.string().alphanum().min(3).max(30),
+  firstName: Joi.string().alphanum().min(3).max(30),
+  lastName: Joi.string().alphanum().min(3).max(30),
   email: Joi.string().email({
     minDomainSegments: 2,
     tlds: { allow: ['com', 'net'] },
   }),
   password: Joi.string().pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')),
-  repeat_password: Joi.ref('password'),
+  repeatPassword: Joi.ref('password'),
 });
 
 module.exports = { userSchema };
