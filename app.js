291e009943225475d5be65e9b3fabd08c9bf823b
alenfador2 <129333585+alenfador2@users.noreply.github.com>
2025-05-12 22:31:30 +0200

    changed cors permissions

--- app.js
+++ app.js
@@ -9,10 +9,10 @@ const app = express();
 
 app.use(
   cors({
-    origin: '*',
+    origin: 'http://localhost:5173',
+    credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: 'include',
   })
 );
 
