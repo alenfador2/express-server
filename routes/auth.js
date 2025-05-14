6791d5db88358599a54ec313c113b0f6805cff71
alenfador2 <129333585+alenfador2@users.noreply.github.com>
2025-05-14 21:32:40 +0200

    delete multer

--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,7 @@
 const { register } = require('../controllers/users.controller');
 const express = require('express');
-const upload = require('../middlewares/multer');
 const router = express.Router();
 
-router.post('/auth/register', upload.single('avatar'), register);
+router.post('/auth/register', register);
 
 module.exports = router;
